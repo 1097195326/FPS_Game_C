36af2b1e1d1d7ca6123753e61c3c7e32195ebb57
1097195326 <1097195326@qq.com>
2016-11-30 09:19:39 +0800

    Initial commit

---
+++ Source/FPS_Game_C/FPS_Game_CGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FPS_Game_CGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_GAME_C_API AFPS_Game_CGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

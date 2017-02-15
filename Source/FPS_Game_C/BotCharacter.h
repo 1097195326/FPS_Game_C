ab36bcd174ea957d6ed664b4e0963f9259d0f5d9
1097195326 <1097195326@qq.com>
2017-02-15 11:41:20 +0800

    add third person run behavir tree

---
+++ Source/FPS_Game_C/BotCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "FPSCharacter.h"
+#include "BotCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_GAME_C_API ABotCharacter : public AFPSCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
